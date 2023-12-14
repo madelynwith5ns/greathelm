436fd4d352bc7ed2517ab7851c595dc9113914c0
MadelynWith5Ns <madelynwith5ns@proton.me>
2023-12-14 16:36:25 -0600

    cosmetic: make term a little prettier

--- src/term.rs
+++ src/term.rs
@@ -1,15 +1,27 @@
 pub fn info(text: String) {
-    println!("\x1b[38;5;27mⓘ  \x1b[1;0m{}", text);
+    println!(
+        "\x1b[38;5;240m[\x1b[38;5;27mINFO \x1b[38;5;240m] \x1b[1;0m{}",
+        text
+    );
 }
 
 pub fn warn(text: String) {
-    eprintln!("\x1b[38;5;214m⚠  \x1b[1;0m{}", text);
+    eprintln!(
+        "\x1b[38;5;240m[\x1b[38;5;214mWARN \x1b[38;5;240m] \x1b[1;0m{}",
+        text
+    );
 }
 
 pub fn error(text: String) {
-    eprintln!("\x1b[38;5;196m⛌  \x1b[1;0m{}", text);
+    eprintln!(
+        "\x1b[38;5;240m[\x1b[38;5;196mERROR\x1b[38;5;240m] \x1b[1;0m{}",
+        text
+    );
 }
 
 pub fn ok(text: String) {
-    println!("\x1b[38;5;40m✔  \x1b[1;0m{}", text);
+    println!(
+        "\x1b[38;5;240m[\x1b[38;5;40mOK   \x1b[38;5;240m] \x1b[1;0m{}",
+        text
+    );
 }
