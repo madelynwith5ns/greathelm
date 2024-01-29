37e3378f3039cdd6f6b199d16adb1a0b987adf84
MadelynWith5Ns <madelynwith5ns@proton.me>
2024-01-29 16:10:06 -0600

    feat: properly finish bringing in modules!

--- scripts/all-plugins.sh
+++ scripts/all-plugins.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/bash
 
-echo Fetching and building all plugins...
-
-if [ -d 'modules' ]; then
-    echo Modules directory exists already. Removing and starting over.
-    rm -rf modules
-fi
-
-if [ -f 'Plugins.ghm' ]; then
-    rm Plugins.ghm
-fi
-
-mkdir modules
-
-echo Fetching: GHP-Rust
-git clone https://github.com/Greathelm/GHP-Rust modules/ghprust
+rm Plugins.ghm || true
+echo Adding GHP-Rust
 echo '@Module ghprust build/ghprust.ghp:build/ghprust.ghp' >> Plugins.ghm
 
 echo Done! Plugins will be built on your next build.
